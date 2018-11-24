5033d813e17db453d0cd7e554962ad9d3ba07ebc
William Prescott <will@theprescotts.com>
2018-11-23 20:54:18 -0500

    Button click works.

--- pipedown/pipedown/ViewController.swift
+++ pipedown/pipedown/ViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var theButton: UIButton?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +21,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func buttonClicked() {
+        print("Button clicked")
+        
+    }
+    
 
 }
 
