a301b367d5f54e057a4c6b4758627003feae745c
Moss Prescott <moss@theprescotts.com>
2018-11-25 17:53:39 -0500

    Play from a buffer (in progress)

--- pipedown/ViewController.swift
+++ pipedown/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     @IBOutlet var startStopButton: UIButton?
@@ -15,7 +16,8 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        setupAudio()
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,14 +33,34 @@ class ViewController: UIViewController {
         if started {
             startStopButton?.setTitleColor(UIColor.green, for: .normal)
             startStopButton?.setTitle("Stop Test", for: .normal)
-//            volumeSlider?.isUserInteractionEnabled = true
             volumeSlider?.isEnabled = true
         }
         else {
             startStopButton?.setTitleColor(UIColor.red, for: .normal)
             startStopButton?.setTitle("Start Test", for: .normal)
-//            volumeSlider?.isUserInteractionEnabled = false
             volumeSlider?.isEnabled = false
+        }
+    }
+    
+    private func setupAudio() {
+        let engine = AVAudioEngine.init()
+        let playerNode = AVAudioPlayerNode()
+        print("outputs: \(playerNode.numberOfOutputs)")
+        print("outputFormat[0]: \(playerNode.outputFormat(forBus: 0))")
+        
+        let sampleRate : Double = 44100.0
+        let buffer = AVAudioPCMBuffer(pcmFormat: AVAudioFormat(standardFormatWithSampleRate: sampleRate, channelLayout: AVAudioChannelLayout(layoutTag: kAudioChannelLayoutTag_Stereo)!), frameCapacity: AVAudioFrameCount(sampleRate/10))!
+        print("buffer.format \(buffer.format)")
+        playerNode.scheduleBuffer(buffer, at: nil, options: .loops, completionHandler: nil)
+
+        engine.attach(playerNode)
+        engine.connect(playerNode, to: engine.mainMixerNode, format: playerNode.outputFormat(forBus: 0))
+        engine.prepare()
+        
+        do {
+            try engine.start()
+        } catch let error {
+            print(error.localizedDescription)
         }
     }
 }
